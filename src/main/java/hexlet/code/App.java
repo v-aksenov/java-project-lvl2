d94cc874b839e509a0f358fc80ef69d3c7175f87
laimo <laimonad@gmail.com>
2021-04-08 21:08:41 +0300

    + @Command

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,8 +1,21 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+
+import java.util.concurrent.Callable;
+
+@CommandLine.Command(name = "main")
+public class App implements Callable<Integer> {
 
     public static void main(final String[] args) {
         System.out.println("Hello World!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        System.out.println("from call");
+        return 0;
     }
 }
